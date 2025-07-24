e3998e58afe3869132a6d083375b29815d584915
mathiasmunene <mathiasmunene@gmail.com>
2025-07-24 22:05:07 +0300

    author and book

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,9 +1,39 @@
 class Author:
-    pass
+    all = []
+
+    def __init__(self, name):
+        if not isinstance(name, str):
+            raise Exception("name must be a string")
+        self.name = name
+        Author.all.append(self)
+
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.author == self]
+
+    def books(self):
+        return [contract.book for contract in self.contracts()]
+
+    def sign_contract(self, book, date, royalties):
+        if not isinstance(book, Book):
+            raise Exception("book must be an instance of Book")
+        if not isinstance(date, str):
+            raise Exception("date must be a string")
+        if not isinstance(royalties, int):
+            raise Exception("royalties must be an integer")
+        return Contract(self, book, date, royalties)
+
+    def total_royalties(self):
+        return sum(contract.royalties for contract in self.contracts())
 
 
 class Book:
-    pass
+    all = []
+
+    def __init__(self, title):
+        if not isinstance(title, str):
+            raise Exception("title must be a string")
+        self.title = title
+        Book.all.append(self)
 
 
 class Contract:
